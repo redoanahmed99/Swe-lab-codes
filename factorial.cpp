52e21fa208f7b981e80493877b7f3e1a81c96f3c
Redoan Ahmed <ahmedredoan90@gmail.com>
2026-07-29 11:57:40 +0600

    Initial commit: factorial program

---
+++ factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long factorial = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n < 0)
+    {
+        cout << "Factorial is not defined for negative numbers.";
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;
+    }
+
+    return 0;
+}
